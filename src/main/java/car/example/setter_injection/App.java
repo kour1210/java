3a7ee055ba7234d2a2281763bbd76cc0362324cd
simkour <simrandeepkour1210@gmail.com>
2025-08-04 14:17:32 +0530

    Add Setter-based dependency injection example using Spring
    - Created car.example.setter_injection package with Car, Specification, and App classes
    - Added applicationSetterInjection.xml for Spring bean configuration
    -The difference was in the Car class using a setter method(here void) whereas there it used simply a constructor
    -In the xml file it had name and ref attributes(property tag) whereas previously it had constructor tag with ref attribute

---
+++ src/main/java/car/example/setter_injection/App.java
@@ -0,0 +1,16 @@
+package car.example.setter_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLOutput;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("applicationSetterInjection.xml");
+        System.out.println("Setter Injection Way");
+        Car myCar = (Car) context.getBean("myCar");
+        myCar.displayDetails();
+    }
+}
