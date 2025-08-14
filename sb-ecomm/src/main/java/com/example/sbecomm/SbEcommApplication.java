a13543ef434790fd5be44e5308032e99b077bb4c
simkour <simrandeepkour1210@gmail.com>
2025-08-14 10:22:00 +0530

    Added sb-ecomm folder for Spring Boot project

---
+++ sb-ecomm/src/main/java/com/example/sbecomm/SbEcommApplication.java
@@ -0,0 +1,14 @@
+package com.example.sbecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcommApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SbEcommApplication.class, args);
+    }
+
+}
